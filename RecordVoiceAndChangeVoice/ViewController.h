e7c2335a17b142d4e615d37576c353cce8e8e9fd
khushiMishtii <khushi.cyber1@gmail.com>
2015-04-23 14:24:41 +0530

    Initial Commit

---
+++ RecordVoiceAndChangeVoice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecordVoiceAndChangeVoice
+//
+//  Created by Khushboo Sharma on 4/23/15.
+//  Copyright (c) 2015 meMy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RecordVoiceAndChangeVoice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RecordVoiceAndChangeVoice
+//
+//  Created by Khushboo Sharma on 4/23/15.
+//  Copyright (c) 2015 meMy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
